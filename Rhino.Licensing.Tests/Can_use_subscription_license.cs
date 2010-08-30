0dc2b634662c9b61bac3375d31c89fa1e0a98084
Hadi Eskandari <h.eskandari@gmail.com>
2010-08-30 14:06:25 +0430

    Skipped tests seem to run fine locally

--- Rhino.Licensing.Tests/Can_use_subscription_license.cs
+++ Rhino.Licensing.Tests/Can_use_subscription_license.cs
@@ -28,7 +28,7 @@ namespace Rhino.Licensing.Tests
   </Signature>
 </license>";
 
-        [Fact(Skip="Needs formatting of output xml")]
+        [Fact]
         public void Can_generate_subscription_license()
         {
             var generator = new LicenseGenerator(public_and_private);
